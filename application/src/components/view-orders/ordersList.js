7fadc8ba7d6518c6f9587f072691553e561e5e93
Francisco Perez <fjperez100@gmail.com>
2023-05-07 16:32:56 -0700

    feat(orderlist-order): replace order jsx with component and pass edit/delete handlers

--- application/src/components/view-orders/ordersList.js
+++ application/src/components/view-orders/ordersList.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import Order from './order';
 
 const OrdersList = (props) => {
-    const { orders } = props;
+    const { orders, onEditOrder, onDeleteOrder } = props;
     if (!props || !props.orders || !props.orders.length) return (
         <div className="empty-orders">
             <h2>There are no orders to display</h2>
@@ -9,24 +10,10 @@ const OrdersList = (props) => {
     );
 
     return orders.map(order => {
-        const createdDate = new Date(order.createdAt);
-        return (
-            <div className="row view-order-container" key={order._id}>
-                <div className="col-md-4 view-order-left-col p-3">
-                    <h2>{order.order_item}</h2>
-                    <p>Ordered by: {order.ordered_by || ''}</p>
-                </div>
-                <div className="col-md-4 d-flex view-order-middle-col">
-                    <p>Order placed at {`${createdDate.getHours()}:${createdDate.getMinutes()}:${createdDate.getSeconds()}`}</p>
-                    <p>Quantity: {order.quantity}</p>
-                </div>
-                <div className="col-md-4 view-order-right-col">
-                    <button className="btn btn-success">Edit</button>
-                    <button className="btn btn-danger">Delete</button>
-                </div>
-            </div>
-        );
-    });
+        return <Order key={order._id}
+            order={order} 
+            onEditOrder={onEditOrder}
+            onDeleteOrder={onDeleteOrder}/>});
 }
 
 export default OrdersList;
