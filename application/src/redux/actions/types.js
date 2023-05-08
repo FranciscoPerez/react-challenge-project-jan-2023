ca02cd0bd9598b6c727b70e8ea0c17dbe74e16f0
Francisco Perez <fjperez100@gmail.com>
2023-05-07 22:13:17 -0700

    feat(action-types): add action types for order redux

--- application/src/redux/actions/types.js
+++ application/src/redux/actions/types.js
@@ -1,2 +1,7 @@
 export const LOGIN = 'login';
 export const LOGOUT = 'logout';
+export const ADD_ORDER = 'add_order';
+export const EDIT_ORDER = 'edit_order';
+export const DELETE_ORDER = 'delete_order';
+export const GET_ORDERS = 'get_orders';
+
