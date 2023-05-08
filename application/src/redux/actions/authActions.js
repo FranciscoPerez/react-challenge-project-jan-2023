6d2b234c859f58505e3c5745c7498343cfcd2c88
Francisco Perez <fjperez100@gmail.com>
2023-05-08 09:13:48 -0700

    refactor(fetch-calls): replace fetch calls with axios calls

--- application/src/redux/actions/authActions.js
+++ application/src/redux/actions/authActions.js
@@ -1,5 +1,7 @@
 import { LOGIN, LOGOUT } from './types';
 import { SERVER_IP } from '../../private'
+import axios from 'axios';
+const LOGIN_URL = `${SERVER_IP}/api/login`;
 
 const finishLogin = (email, token) => {
     return {
@@ -13,17 +15,11 @@ const finishLogin = (email, token) => {
 
 export const loginUser = (email, password) => {
     return (dispatch) => {
-        fetch(`${SERVER_IP}/api/login`, {
-            method: 'POST',
-            body: JSON.stringify({
-                email,
-                password
-            }),
-            headers: {
-                'Content-Type': 'application/json'
-            },
-        }).then(response => response.json())
-        .then(response => {
+        axios.post(LOGIN_URL, {
+            email,
+            password
+        })
+        .then(({data: response}) => {
             if (response.success) {
                 dispatch(finishLogin(response.email, response.token));
             }
--- application/src/redux/actions/orderActions.js
+++ application/src/redux/actions/orderActions.js
@@ -1,5 +1,6 @@
 import { SERVER_IP } from "../../private";
 import { ADD_ORDER, EDIT_ORDER, DELETE_ORDER, GET_ORDERS } from "./types";
+import axios from 'axios';
 
 // declare api url variables
 const ADD_ORDER_URL = `${SERVER_IP}/api/add-order`;
@@ -9,70 +10,47 @@ const GET_ORDERS_URL = `${SERVER_IP}/api/current-orders`;
 
 export const addOrder = (order) => {
   return (dispatch) => {
-    fetch(ADD_ORDER_URL, {
-      method: "POST",
-      body: JSON.stringify({
-        order_item: order.order_item,
-        quantity: order.quantity,
-        ordered_by: order.ordered_by,
-      }),
-      headers: {
-        "Content-Type": "application/json",
-      },
+    axios.post(ADD_ORDER_URL, {
+      order_item: order.order_item,
+      quantity: order.quantity,
+      ordered_by: order.ordered_by,
     })
-      .then((res) => res.json())
-      .then((response) => {
-        console.log("Success", JSON.stringify(response));
-        dispatch({
-          type: ADD_ORDER,
-          payload: {
-            order: {
-              ...order,
-              _id: response.insertedId,
-            },
+    .then(({data: response}) => {
+      console.log("Success", JSON.stringify(response));
+      dispatch({
+        type: ADD_ORDER,
+        payload: {
+          order: {
+            ...order,
+            _id: response.insertedId,
           },
-        });
-      })
-      .catch((error) => console.error(error));
+        },
+      });
+    })
+    .catch((error) => console.error(error));
   };
 };
 
 export const editOrder = (order) => {
   return (dispatch) => {
-    fetch(EDIT_ORDER_URL, {
-      method: "POST",
-      body: JSON.stringify({
-        id: order.id,
-        order_item: order.order_item,
-        quantity: order.quantity,
-        ordered_by: order.ordered_by,
-      }),
-      headers: {
-        "Content-Type": "application/json",
-      },
+    axios.post(EDIT_ORDER_URL, {
+      id: order.id,
+      order_item: order.order_item,
+      quantity: order.quantity,
+      ordered_by: order.ordered_by,
     })
-      .then((res) => res.json())
-      .then((response) => {
-        console.log("Success", JSON.stringify(response));
-        dispatch({ type: EDIT_ORDER, payload: { order } });
-      })
-      .catch((error) => console.error(error));
+    .then(({data: response}) => {
+      console.log("Success", JSON.stringify(response));
+      dispatch({ type: EDIT_ORDER, payload: { order } });
+    })
+    .catch((error) => console.error(error));
   };
 };
 
 export const deleteOrder = (id) => {
   return (dispatch) => {
-    fetch(DELETE_ORDER_URL, {
-      method: "POST",
-      body: JSON.stringify({
-        id: id,
-      }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then((res) => res.json())
-      .then((response) => {
+    axios.post(DELETE_ORDER_URL, {id})
+      .then(({data: response}) => {
         console.log("Success", JSON.stringify(response));
         dispatch({ type: DELETE_ORDER, payload: { id } });
       })
@@ -82,14 +60,14 @@ export const deleteOrder = (id) => {
 
 export const getOrders = (order) => {
   return (dispatch) => {
-    fetch(GET_ORDERS_URL)
-      .then((response) => response.json())
-      .then((response) => {
+
+    axios.get(GET_ORDERS_URL)
+      .then(({data: response}) => {
         if (response.success) {
           dispatch({ type: GET_ORDERS, payload: { orders: response.orders } });
         } else {
           console.log("Error getting orders");
         }
-      });
+      })
   };
 };
