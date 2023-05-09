c1351d1d8ad0e409e9a1699c85df2dd098d06210
Francisco Perez <fjperez100@gmail.com>
2023-05-08 23:36:33 -0700

    feat(app-router): add create-account/register route

--- application/src/router/appRouter.js
+++ application/src/router/appRouter.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { Main, Login, OrderForm, ViewOrders} from '../components';
+import { Main, Login, OrderForm, ViewOrders, Register} from '../components';
 
 const AppRouter = (props) => {
   return (
@@ -9,6 +9,7 @@ const AppRouter = (props) => {
       <Route path="/login" exact component={Login} />
       <Route path="/order" exact component={OrderForm} />
       <Route path="/view-orders" exact component={ViewOrders} />
+      <Route path="/create-account" exact component={Register} />
     </Router>
   );
 }
