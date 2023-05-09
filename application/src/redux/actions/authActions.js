89dd007dd1b4f1b578fc71483267f803cdd46206
Francisco Perez <fjperez100@gmail.com>
2023-05-08 23:46:59 -0700

    feat(auth-thunk): add database and redirect functionality to login thunk

--- application/src/redux/actions/authActions.js
+++ application/src/redux/actions/authActions.js
@@ -11,8 +11,9 @@ const finishLogin = (email, token) => {
     }
 }
 
-export const loginUser = (email, password) => {
+export const loginUser = (email, password, history, setSubmitError) => {
     return (dispatch) => {
+        setSubmitError('');
         fetch(`${SERVER_IP}/api/login`, {
             method: 'POST',
             body: JSON.stringify({
@@ -22,11 +23,19 @@ export const loginUser = (email, password) => {
             headers: {
                 'Content-Type': 'application/json'
             },
-        }).then(response => response.json())
+        })
+        .then(response => response.json())
         .then(response => {
             if (response.success) {
                 dispatch(finishLogin(response.email, response.token));
+                history.push('/view-orders')
+            } else {
+                setSubmitError(response.error);
             }
+        })
+        .catch((error) => {
+            console.log(error);
+            setSubmitError('Login failed');
         })
     };
 }
