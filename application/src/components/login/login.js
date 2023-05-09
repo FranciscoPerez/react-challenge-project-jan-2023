e612894164eb5b418446d938959615dc537e7a8d
Francisco Perez <fjperez100@gmail.com>
2023-05-08 23:44:46 -0700

    feat(login-form): add database login functionality and simple user feedback

--- application/src/components/login/login.js
+++ application/src/components/login/login.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import LoginForm from './login-form/loginForm';
 import './login.css';
+import { Link } from 'react-router-dom';
 
 class Login extends Component {
   
@@ -9,8 +10,9 @@ class Login extends Component {
       <div className="main-body">
         <h1 className="text-center">Login Screen</h1>
         <div className="d-flex justify-content-center mt-5">
-          <LoginForm onLogin={() => {this.props.history.push('/view-orders')}}/>
+          <LoginForm />
         </div>
+        <Link to={"/create-account"}>Create Account</Link>
       </div>
     )
   }
